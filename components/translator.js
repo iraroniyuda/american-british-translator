6af2837be8c5944a652bff85c14160548b804340
Ira Roni Yuda <iraroniyuda@gmail.com>
2026-06-14 11:15:21 +0700

    complete american british translator

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,114 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+"use strict";
+
+const americanOnly = require("./american-only.js");
+const britishOnly = require("./british-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
 
 class Translator {
+  constructor() {
+    this.americanToBritish = {
+      ...americanOnly,
+      ...americanToBritishSpelling,
+      ...americanToBritishTitles,
+    };
 
+    this.britishToAmerican = {
+      ...britishOnly,
+      ...this.reverseDictionary(americanToBritishSpelling),
+      ...this.reverseDictionary(americanToBritishTitles),
+    };
+  }
+
+  reverseDictionary(dictionary) {
+    const reversed = {};
+
+    Object.keys(dictionary).forEach((key) => {
+      reversed[dictionary[key]] = key;
+    });
+
+    return reversed;
+  }
+
+  escapeRegex(text) {
+    return text.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  }
+
+  preserveCase(original, translated) {
+    if (!original || !translated) {
+      return translated;
+    }
+
+    if (original[0] === original[0].toUpperCase()) {
+      return translated[0].toUpperCase() + translated.slice(1);
+    }
+
+    return translated;
+  }
+
+  highlight(text) {
+    return `<span class="highlight">${text}</span>`;
+  }
+
+  replaceTerms(text, dictionary, shouldHighlight) {
+    let translatedText = text;
+
+    const terms = Object.keys(dictionary).sort((a, b) => b.length - a.length);
+
+    terms.forEach((term) => {
+      const escapedTerm = this.escapeRegex(term);
+      const regex = new RegExp(`(^|[^A-Za-z0-9])(${escapedTerm})(?=$|[^A-Za-z0-9])`, "gi");
+
+      translatedText = translatedText.replace(regex, (match, prefix, foundTerm) => {
+        let replacement = dictionary[term.toLowerCase()] || dictionary[term];
+
+        replacement = this.preserveCase(foundTerm, replacement);
+
+        if (shouldHighlight) {
+          replacement = this.highlight(replacement);
+        }
+
+        return prefix + replacement;
+      });
+    });
+
+    return translatedText;
+  }
+
+  replaceTime(text, locale, shouldHighlight) {
+    if (locale === "american-to-british") {
+      return text.replace(/\b([0-9]{1,2}):([0-9]{2})\b/g, (match, hour, minute) => {
+        const replacement = `${hour}.${minute}`;
+        return shouldHighlight ? this.highlight(replacement) : replacement;
+      });
+    }
+
+    if (locale === "british-to-american") {
+      return text.replace(/\b([0-9]{1,2})\.([0-9]{2})\b/g, (match, hour, minute) => {
+        const replacement = `${hour}:${minute}`;
+        return shouldHighlight ? this.highlight(replacement) : replacement;
+      });
+    }
+
+    return text;
+  }
+
+  translate(text, locale, shouldHighlight = true) {
+    let dictionary;
+
+    if (locale === "american-to-british") {
+      dictionary = this.americanToBritish;
+    } else if (locale === "british-to-american") {
+      dictionary = this.britishToAmerican;
+    } else {
+      return text;
+    }
+
+    let translatedText = this.replaceTerms(text, dictionary, shouldHighlight);
+    translatedText = this.replaceTime(translatedText, locale, shouldHighlight);
+
+    return translatedText;
+  }
 }
 
 module.exports = Translator;
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,111 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server.js');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
+suite("Functional Tests", function () {
+  test("Translation with text and locale fields: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "Mangoes are my favorite fruit.",
+        locale: "american-to-british",
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
 
-suite('Functional Tests', () => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.text, "Mangoes are my favorite fruit.");
+        assert.equal(
+          res.body.translation,
+          'Mangoes are my <span class="highlight">favourite</span> fruit.'
+        );
 
+        done();
+      });
+  });
+
+  test("Translation with text and invalid locale field: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "Mangoes are my favorite fruit.",
+        locale: "invalid-locale",
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
+
+        assert.deepEqual(res.body, { error: "Invalid value for locale field" });
+        done();
+      });
+  });
+
+  test("Translation with missing text field: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        locale: "american-to-british",
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
+
+        assert.deepEqual(res.body, { error: "Required field(s) missing" });
+        done();
+      });
+  });
+
+  test("Translation with missing locale field: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "Mangoes are my favorite fruit.",
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
+
+        assert.deepEqual(res.body, { error: "Required field(s) missing" });
+        done();
+      });
+  });
+
+  test("Translation with empty text: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "",
+        locale: "american-to-british",
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
+
+        assert.deepEqual(res.body, { error: "No text to translate" });
+        done();
+      });
+  });
+
+  test("Translation with text that needs no translation: POST request to /api/translate", function (done) {
+    chai
+      .request(server)
+      .post("/api/translate")
+      .send({
+        text: "This sentence is already fine.",
+        locale: "american-to-british",
+      })
+      .end(function (err, res) {
+        if (err) return done(err);
+
+        assert.equal(res.status, 200);
+        assert.equal(res.body.text, "This sentence is already fine.");
+        assert.equal(res.body.translation, "Everything looks good to me!");
+
+        done();
+      });
+  });
 });
